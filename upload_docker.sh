d3d3f8842564b02c6997da1b8406b8c35037f3bd
okikiolumide <30922643+okikiolumide@users.noreply.github.com>
2022-08-15 21:33:43 +0100

    upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=prediction-api:1.0
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag prediction-api:1.0 kikiolu25/prediction-api:1.0
+docker login -u kikiolu25 -p $PASSWORD
 
 # Step 3:
 # Push image to a docker repository
+docker push kikiolu25/prediction-api:1.0
